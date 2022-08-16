e5815fa3a3ccf6e42a8d27791a6154b81ac82124
Leximiller128 <leximiller128@gmail.com>
2022-08-16 14:28:08 -0600

    js markdown created

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,33 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title} 
+  ## License Badge 
+  ${data.license}
+  ## Description 
+  ${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests) 
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Contributing 
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have questions about this project, you can reach me at ${data.email} or my github ${data.githubUserName}.
 `;
 }
 
