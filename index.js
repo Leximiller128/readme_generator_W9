5ad2b38f74d18a0a5d89b0b45a3917587b0fc01c
Leximiller128 <leximiller128@gmail.com>
2022-08-16 18:10:17 -0600

    built functions in index. Added npm

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions for this project?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      console.log(answers);
+      writeToFile("README.md", generateMarkdown(answers));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 // Function call to initialize app
 init();
